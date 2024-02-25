b7115a1af91cf1476cdf470cfcb495bfc8888a55
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-02-26 00:13:48 +0100

    first implementation

---
+++ src/Artemis.Plugins.AuroraxArtemis/AuroraInterface.cs
@@ -0,0 +1,43 @@
+﻿using System.IO.Pipes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artemis.Plugins.AuroraxArtemis;
+
+public class AuroraInterface
+{
+    private readonly byte[] _end = "\n"u8.ToArray();
+    
+    public async Task ShutdownAuroraDevices()
+    {
+        var sendCommand = SendCommand("quitDevices");
+        
+        await Task.WhenAny(Task.Delay(2), sendCommand);
+    }
+    
+    public async Task StartAuroraDevices()
+    {
+        var sendCommand = SendCommand("startDevices");
+
+        await Task.WhenAny(Task.Delay(2), sendCommand);
+    }
+
+    private Task SendCommand(string command)
+    {
+        return SendCommand(Encoding.UTF8.GetBytes(command));
+    }
+
+    private async Task SendCommand(byte[] command)
+    {
+        var client = new NamedPipeClientStream(".", "aurora\\interface", PipeDirection.Out, PipeOptions.Asynchronous);
+        await client.ConnectAsync(2000);
+        if (!client.IsConnected)
+            return;
+        
+        client.Write(command, 0, command.Length);
+        client.Write(_end, 0, _end.Length);
+        
+        client.Flush();
+        client.Close();
+    }
+}
--- src/Artemis.Plugins.AuroraxArtemis/Bootstrapper.cs
+++ src/Artemis.Plugins.AuroraxArtemis/Bootstrapper.cs
@@ -1,22 +1,23 @@
 using Artemis.Core;
-using Artemis.UI.Shared;
 
-namespace Artemis.Plugins.xAurora;
+namespace Artemis.Plugins.AuroraxArtemis;
 
 public class Bootstrapper : PluginBootstrapper
 {
+    private readonly AuroraInterface _auroraInterface = new();
+    
     public override void OnPluginLoaded(Plugin plugin)
     {
-        
+        //unused
     }
 
     public override void OnPluginEnabled(Plugin plugin)
     {
-        
+        _auroraInterface.ShutdownAuroraDevices().Wait();
     }
     
     public override void OnPluginDisabled(Plugin plugin)
     {
-        
+        _auroraInterface.StartAuroraDevices().Wait();
     }
 }
